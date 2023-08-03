38af64ab3cd8ce912c69c2f01bd1bb383a264648
Ninth <quarpoe@gmail.com>
2023-08-04 00:38:49 +0300

    feat(Commit.php): add OpenAI dependency and generate commit message using git diff

--- app/Commands/Commit.php
+++ app/Commands/Commit.php
@@ -2,6 +2,7 @@
 
 namespace App\Commands;
 
+use App\OpenAI;
 use LaravelZero\Framework\Commands\Command;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
@@ -17,6 +18,14 @@ class Commit extends Command
      * The description of the command.
      */
     protected $description = 'Automatically generate commit messages';
+
+    private $openAi;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->openAi = new OpenAI(env('API_KEY'));
+    }
 
     /**
      * Execute the console command.
@@ -54,7 +63,24 @@ class Commit extends Command
      */
     private function generateCommitMessage(string $gitDiff): string
     {
-        return 'result';
+        return $this->openAi->complete([
+            [
+                'role' => 'system',
+                'content' => "You are to act as the author of a commit message in git. Your task is to create a clean and comprehensive commit message using conventional commit conventions. I'\''ll send you the output of a '\''git diff --staged'\'' command, and you will convert it into a commit message. Do not preface the commit with anything, use the present tense. Don'\''t add any descriptions to the commit, just the commit message. The first line should be no longer than 50 characters, and the body should be limited to 72 characters. Reply in English.",
+            ],
+            [
+                'role' => 'user',
+                'content' => 'diff --git a/src/server.ts b/src/server.ts\n    index ad4db42..f3b18a9 100644\n    --- a/src/server.ts\n    +++ b/src/server.ts\n    @@ -10,7 +10,7 @@ import {\n      initWinstonLogger();\n      \n      const app = express();\n    -const port = 7799;\n    +const PORT = 7799;\n      \n      app.use(express.json());\n      \n    @@ -34,6 +34,6 @@ app.use((_, res, next) => {\n      // ROUTES\n      app.use(PROTECTED_ROUTER_URL, protectedRouter);\n      \n    -app.listen(port, () => {\n    -  console.log(\\`Server listening on port \\{$port}\\`);\n    +app.listen(process.env.PORT || PORT, () => {\n    +  console.log(\\`Server listening on port \\{$PORT}\\`);\n      });',
+            ],
+            [
+                'role' => 'assistant',
+                'content' => 'feat(server.ts): add support for process.env.PORT environment variable and change port variable case from lowercase port to uppercase PORT',
+            ],
+            [
+                'role' => 'user',
+                'content' => $gitDiff,
+            ],
+        ]);
     }
 
     /**
