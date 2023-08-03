8fd98db080deabe7504744734d7e5d85c409effb
Ninth <quarpoe@gmail.com>
2023-08-04 00:38:39 +0300

    refactor(OpenAI.php): refactor OpenAI class to add support for API URL constant, use Illuminate\Support\Facades\Http for HTTP requests, and update model property to use GPTModels::GPT3_16K->value

--- app/OpenAI.php
+++ app/OpenAI.php
@@ -3,14 +3,42 @@
 namespace App;
 
 use App\Enums\GPTModels;
+use Illuminate\Support\Facades\Http;
 
 class OpenAI
 {
+    protected const API_URL = 'https://api.openai.com/v1/chat/completions';
+
     public function __construct(
         protected string $apiKey,
-        protected string $model = GPTModels::GPT3_16K,
+        protected string $model = GPTModels::GPT3_16K->value,
         protected float $temperature = 0.2,
         protected int $maxTokens = 196
     ) {
+    }
+
+    public function complete(array $messages): string
+    {
+        $response = Http::withToken(
+            token: $this->apiKey
+        )->post(
+            url: OpenAI::API_URL,
+            data: $this->prepareData(messages: $messages)
+        )->throw();
+
+        return $response['choices'][0]['message']['content'];
+    }
+
+    protected function prepareData(array $messages): array
+    {
+        return [
+            'model' => $this->model,
+            'messages' => $messages,
+            'temperature' => $this->temperature,
+            'max_tokens' => $this->maxTokens,
+            'top_p' => 1,
+            'frequency_penalty' => 0,
+            'presence_penalty' => 0,
+        ];
     }
 }
