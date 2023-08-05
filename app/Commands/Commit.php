b91fbd2f85d1e9a91ae3f00d8f1e3d212a139c7f
Ninth <quarpoe@gmail.com>
2023-08-06 01:03:14 +0300

    refactor(Commit.php): refactor class for better readability and maintanability

--- app/Commands/Commit.php
+++ app/Commands/Commit.php
@@ -11,16 +11,33 @@ class Commit extends Command
 {
     /**
      * The signature of the command.
+     *
+     * @var string
      */
     protected $signature = 'commit';
 
     /**
      * The description of the command.
+     *
+     * @var string
      */
     protected $description = 'Automatically generate commit messages';
 
+    /**
+     * This is a private variable that stores the OpenAI instance.
+     *
+     * @var OpenAI
+     */
     private $openAi;
 
+    /**
+     * Constructor for the class.
+     *
+     * Initializes the object by calling the parent constructor and creating
+     * a new instance of the OpenAI class with the API key from the environment.
+     *
+     * @return void
+     */
     public function __construct()
     {
         parent::__construct();
@@ -28,23 +45,26 @@ class Commit extends Command
     }
 
     /**
-     * Execute the console command.
-     *
-     * @return mixed
+     * Handles the logic for the Command.
      */
     public function handle()
     {
         try {
-            $gitDiff = $this->getGitDiff();
-            $commitMessage = $this->generateCommitMessage($gitDiff);
-            $this->handleUserResponse($commitMessage);
+            $diff = $this->getGitDiff();
+            $message = $this->generateCommitMessage($diff);
+            $this->handleUserResponse($message);
         } catch (\Exception $e) {
             $this->error($e->getMessage());
         }
     }
 
     /**
-     * Get the staged git diff.
+     * Retrieves the git diff of the staged changes.
+     *
+     * @return string the git diff output
+     *
+     * @throws ProcessFailedException if the git diff command fails
+     * @throws \Exception             if there are no staged changes
      */
     private function getGitDiff(): string
     {
@@ -57,7 +77,7 @@ class Commit extends Command
 
         $output = $process->getOutput();
 
-        if ($output === '') {
+        if (empty($output)) {
             throw new \Exception('There are no changes yet!');
         }
 
@@ -65,9 +85,13 @@ class Commit extends Command
     }
 
     /**
-     * Generate the commit message using the OpenAI API.
+     * Generate a commit message based on the output of a git diff command.
+     *
+     * @param string $diff the output of a git diff command
+     *
+     * @return string the generated commit message
      */
-    private function generateCommitMessage(string $gitDiff): string
+    private function generateCommitMessage(string $diff): string
     {
         return $this->openAi->complete([
             [
@@ -84,36 +108,94 @@ class Commit extends Command
             ],
             [
                 'role' => 'user',
-                'content' => $gitDiff,
+                'content' => $diff,
             ],
         ]);
     }
 
     /**
-     * Handle the user's response to the generated commit message.
+     * Handles the user response and performs the necessary actions.
+     *
+     * @param string $message the commit response message
+     *
+     * @throws ProcessFailedException if the process of committing is not successful
      */
-    private function handleUserResponse(string $commitMessage): void
+    private function handleUserResponse(string $message): void
     {
-        $this->info('Here is the generated commit message:');
-        $this->line($commitMessage);
-        $this->newLine();
+        $this->warn($message);
 
-        if ($this->confirm('Do you want to modify it?')) {
-            $commitMessage = $this->anticipate('Pease enter the new commit message. Use TAB to autocomplete', [$commitMessage]);
+        if ($this->shouldModifyCommit($message)) {
+            $message = $this->getNewCommitMessage($message);
+            $this->warn($message);
         }
 
-        if ($this->confirm("Do you accept this commit message?\n{$commitMessage}\n", true)) {
-            $process = new Process(['git', 'commit', '-m', $commitMessage]);
-            $process->setTty(true);
-            $process->run();
+        if ($this->confirmCommit($message)) {
+            $this->commitChanges($message);
 
-            if (!$process->isSuccessful()) {
-                throw new ProcessFailedException($process);
-            }
-
-            $this->info('Commit successful!');
-        } else {
-            $this->info('Commit message discarded.');
+            return;
         }
+
+        $this->discardCommit();
+    }
+
+    /**
+     * Checks if the user wants to modify the commit message.
+     *
+     * @param string $message the commit response message
+     */
+    private function shouldModifyCommit(string $message): bool
+    {
+        return $this->confirm('Do you want to modify it?');
+    }
+
+    /**
+     * Prompts the user for a new commit message.
+     *
+     * @param string $message the commit response message
+     */
+    private function getNewCommitMessage(string $message): string
+    {
+        return $this->anticipate(
+            'Please enter the new commit message. Use TAB to autocomplete',
+            [$message]
+        );
+    }
+
+    /**
+     * Confirms if the user accepts the commit message.
+     */
+    private function confirmCommit(): bool
+    {
+        $confirmationMessage = 'Do you accept this commit message?';
+
+        return $this->confirm($confirmationMessage, true);
+    }
+
+    /**
+     * Commits the changes using the given message.
+     *
+     * @param string $message the commit message
+     *
+     * @throws ProcessFailedException if the process of committing is not successful
+     */
+    private function commitChanges(string $message): void
+    {
+        $command = ['git', 'commit', '-m', $message];
+        $process = new Process($command);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        $this->info('Commit successful!');
+    }
+
+    /**
+     * Discards the commit message.
+     */
+    private function discardCommit(): void
+    {
+        $this->info('Commit message discarded.');
     }
 }
