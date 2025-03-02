2bafa866c1d0982b72efef79b58901def49138f2
Dominik Lokkhart <lokkhart@yard77.dev>
2025-03-02 14:31:48 +0200

    Update prompt to not explain or assume why the change was made

--- app/Prompt.php
+++ app/Prompt.php
@@ -45,6 +45,7 @@ class Prompt
         Output Instructions:
         - You only output human readable Markdown, except for the links, which should be in HTML format.
         - You do not format your commit as a block of code.
+        - You do not explain or assume why the change was made. Just write what change was made.
         - You only describe your changes in imperative mood, e.g. "Make xyzzy do frotz" instead of "[This patch] makes xyzzy do frotz" or "[I] changed xyzzy to do frotz", as if you are giving orders to the codebase to change its behavior. Try to make sure your explanation can be understood without external resources.
         - You do not use the past tense, only the present tense.
         - You do not preface your commit with anything.
